817eb87662bef893d82bf8e6f417105a42f7858f
Tom Huynh <tom.huynh@rmit.edu.vn>
2024-07-22 09:03:33 +0700

    Initial Commit

---
+++ My Contact List/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  My Contact List
+//
+//  Created by Tom Huynh on 22/7/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
